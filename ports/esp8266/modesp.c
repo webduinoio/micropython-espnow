4c74d11d9db541c4c44cda34a4ba26e480c34e28
glenn20 <glenn.moloney@gmail.com>
2020-10-19 15:28:57 +1100

    esp/espnow: Support buffer and stream IO.
    Update for:
     - v1.13 and v1.14 compatible
     - Ring buffers in esp{32,8266}/ringbuffer.[ch] for robust IO.
     - Allocate the ESPNow singleton dynamically (protect bufs from gc).
     - Docs provided at docs/library/espnow.rst.
    
    Remove:
     - on_recv() and on_send(): use of "sched" callbacks is unnecessary
       in this use case and just adds fragility.
    
    New methods:
     - recv() to read incoming messages (instead of callbacks).
     - irecv() for allocation-free read of incoming messages.
     - Stream IO support: read(), write() and poll through ioctl(). This
       can be used to support asyncio use of espnow.
     - Also added read1() and readinto1().
     - Add support for iteration on ESPNow object for alloc-free read.
     - config(): set rx buffer sizes and read timeout
     - stats(): Returns transfer stats:
         (tx_pkts, tx_pkt_responses, tx_failures, rx_pkts, lost_rx_pkts).
     - get_peer(mac): Return peer info: (mac,lmk,channel,ifidx,encrypt)
     - mod_peer(mac, ...) to change peer info parameters.
     - get_peers(): to return all peer info tuples.
    Deleted:
     - espnow_lmk() which is subsumed by mod_peer().
    
    Implementation changes:
     - send(): Remove automatic changes to ifidx parameter for peers. This
       is better managed in the app layer since we now provide access to
       the peer parameters.
     - Use the new consolidated check_esp_err(0 for exception
    
    Add new constants as attributes of the ESPNow object:
     - MAX_DATA_LEN         (=ESP_NOW_MAX_DATA_LEN) (250)
     - KEY_LEN              (=ESP_NOW_KEY_LEN) (16)
     - MAX_TOTAL_PEER_NUM   (=ESP_NOW_MAX_TOTAL_PEER_NUM) (20)
     - MAX_ENCRYPT_PEER_NUM (=ESP_NOW_MAX_ENCRYPT_PEER_NUM) (6)
    
    ESP8266 is a pared down version of the esp32 espnow support due to code
    size restrictions in the GENERIC_1M target. See docs for details.
    There is NO support for the stream IO functions or asyncio.

--- ports/esp8266/modesp.c
+++ ports/esp8266/modesp.c
@@ -351,7 +351,9 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(esp_set_native_code_location_obj, esp_set_nativ
 
 #endif
 
+#if MICROPY_ESP8266_ESPNOW
 extern const mp_obj_module_t mp_module_esp_espnow;
+#endif
 
 STATIC const mp_rom_map_elem_t esp_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_esp) },
@@ -389,7 +391,9 @@ STATIC const mp_rom_map_elem_t esp_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SLEEP_MODEM), MP_ROM_INT(MODEM_SLEEP_T) },
     #endif
 
+    #if MICROPY_ESP8266_ESPNOW
     { MP_ROM_QSTR(MP_QSTR_espnow), MP_ROM_PTR(&mp_module_esp_espnow) },
+    #endif
 };
 
 STATIC MP_DEFINE_CONST_DICT(esp_module_globals, esp_module_globals_table);
